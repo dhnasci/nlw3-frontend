1ca421741e63b382221d7bf92332f1bf32dc1934
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-16 18:11:18 -0400

    Landing page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-interface TitleProps {
-  text: string;
-}
+import { FiArrowRight } from 'react-icons/fi';
+import './styles/global.css'
+import './styles/pages/landing.css'
 
-function Title(props: TitleProps) {
-  return <h1>{ props.text} </h1>
-}
+import logoImg from './images/logo.svg'
 
 function App() {
-  return (
-    <div className="App">
-     <Title text="Titulo 1" />
-     <Title text="Titulo 2" />
-     <Title text="Titulo 3" />
-     <Title text="Titulo 4" />
-     <Title text="Titulo 5" />
+  return (<div id="page-landing">
+       <div className="content-wrapper">
+          <img src={logoImg} alt="" />
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+
+          <div className="location">
+            <strong>Manaus</strong>
+            <span>Amazonas</span>
+          </div>
+
+          <a href="https://" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+          </a>
+       </div>
     </div>
-  );
+  )
 }
 
 export default App;
